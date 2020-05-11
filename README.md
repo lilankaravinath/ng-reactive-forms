ff82be19ccc9e0397f6d73725b4d0739bdd86940
Lilanka Ravinath <inbox.lilanka@gmail.com>
2020-05-11 18:50:36 +0530

    Finalise the Project

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
 # NgReactiveForms
+Handling user input with forms is the cornerstone of many common applications. Applications use forms to enable users to log in, to update a profile, to enter sensitive information, and to perform many other data-entry tasks.
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.4.
+Angular provides two different approaches to handling user input through forms: `reactive` and `template-driven`. Both capture user input events from the view, validate the user input, create a form model and data model to update, and provide a way to track changes.
+
+`Reactive` and `template-driven` forms process and manage form data differently. Each offers different advantages.
+
+## Reactive Forms
+
+In general: Reactive forms are more robust: they're more scalable, reusable, and testable. If forms are a key part of your application, or you're already using reactive patterns for building your application, use reactive forms.
+
+## Project
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.4 for learning reactive forms.
+
+## How to use
+
+* Run "npm install" inside this project folder to install all dependencies.
+
+* Make sure you use the relavent version of the CLI
+
+* Run "ng serve" to see the app in action (try "npm start" in case "ng serve" fails).
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
 ## Further help
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-reactive-forms';
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signUpForm: FormGroup;
+  forbiddenUserNames = ['Chris', 'Anna'];
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    });
+
+    // for subscribing valueChanges
+    // this.signUpForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // );
+    // for subscribing statusChanges
+    this.signUpForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    );
+
+    // Set Values
+    this.signUpForm.setValue({
+      'userData': {
+        'username': 'Abey',
+        'email': 'abey@test.com'
+      },
+      'gender': 'male',
+      'hobbies': []
+    });
+
+    // Patch Value
+    this.signUpForm.setValue({
+      'userData': {
+        'username': 'Rithu'
+      }
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+
+    // You can pass an object to reset() to reset to specific values.
+    this.signUpForm.reset();
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+
+  getControls() {
+    return (<FormArray>this.signUpForm.get('hobbies')).controls;
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return {'nameIsFrobidden': true};
+    }
+    return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
 }
